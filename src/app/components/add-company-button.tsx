b378faa5ec07350117ce8a69d576b2c8fbc95164
Daryna Holikova <dgolikova0@gmail.com>
2024-12-08 16:13:30 +0000

    add company button to open modal

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React, {useState} from 'react'
+import {Button} from "@headlessui/react";
+import CompanyFormModal from "@/src/app/components/company-form-modal";
+
+export default function AddCompanyButton() {
+    const [show, setShow] = useState(false);
+    return (
+        <>
+            <Button onClick={() => setShow(true)}  className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">Add company</Button>
+            <CompanyFormModal
+                onSubmit={console.log}
+                show={show}
+                onClose={() => setShow(false)}
+            />
+        </>
+    );
+}
