63e5f7798926875802d7ff923a5f75616476fe42
Daryna Holikova <dgolikova0@gmail.com>
2024-12-08 16:13:02 +0000

    add input-field and logo-uploader components

--- src/app/components/input-field.tsx
+++ src/app/components/input-field.tsx
@@ -1,12 +1,11 @@
-'use client'
-import React from 'react'
-import {Field} from "formik";
-
-export interface InputFieldProps {
+'use client';
+import React from 'react';
+import { Field } from 'formik';
+export interface InputFieldProps
+    extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
 }
-
-export default function InputField({label, id, ...rest} : InputFieldProps) {
+export default function InputField({ label, id, ...rest }: InputFieldProps) {
     return (
         <div className="flex flex-col">
             {label && (
@@ -17,8 +16,8 @@ export default function InputField({label, id, ...rest} : InputFieldProps) {
             <Field
                 {...rest}
                 id={id}
-                classname="p-3 h-11 text-sm rounded border border-gray-300 shadow"
+                className="p-3 h-11 text-sm rounded border border-gray-300 shadow"
             />
         </div>
-    )
+    );
 }
