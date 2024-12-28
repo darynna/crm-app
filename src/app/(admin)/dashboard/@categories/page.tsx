6551d9383c75fbb982e1bf9fe447c1868839f9b0
Daryna Holikova <dgolikova0@gmail.com>
2024-12-28 23:39:20 +0000

    Learning fetch data

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { getSummaryCategories } from '@/src/lib/api';
+import { getCategories, getCompanies } from '@/src/lib/api';
+import getCountById from '@/src/lib/utils/getCountById';
 import StatCard, { StatCardType } from '@/src/app/components/stat-card';
 import DashboardCard from '@/src/app/components/dashboard-card';
 
 export default async function Page() {
-  const data = await getSummaryCategories();
+  const categories = await getCategories();
+  const companies = await getCompanies();
+
+  const counts = getCountById(companies, 'categoryId');
   return (
     <DashboardCard label="Categories of companies">
       <div className="grid grid-cols-12 gap-3 pb-5 px-5">
-        {data.map(({ categoryId, categoryTitle, count }) => (
-          <div key={categoryId} className="col-span-3">
+        {categories.map(({ id, title }) => (
+          <div key={id} className="col-span-3">
             <StatCard
               type={StatCardType.Dark}
-              label={categoryTitle}
-              counter={count}
+              label={title}
+              counter={counts[id] || 0}
             />
           </div>
         ))}
--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { getSummaryStats } from '@/src/lib/api';
+import { getSummaryStats, SummaryStats } from '@/src/lib/api';
 import StatCard, { StatCardType } from '@/src/app/components/stat-card';
-import Link from 'next/link';
-const labelByStat = {
+const labelByStat: Record<keyof SummaryStats, string> = {
   promotions: 'Total promotions',
   categories: 'Total categories',
   newCompanies: 'New companies',
   activeCompanies: 'Total active companies',
 };
 export default async function Page() {
-  const data = await getSummaryStats();
+  const data = await getSummaryStats({
+    next: {
+      revalidate: 5
+    }
+  });
   return (
     <div className="grid grid-cols-12 gap-5">
-      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
-        <Link href={`/dashboard/${key}`} key={key} className="col-span-3">
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map((key) => (
+        <div key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
             label={labelByStat[key]}
             counter={data[key]}
           />
-        </Link>
+        </div>
       ))}
     </div>
   );
