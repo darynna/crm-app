f8ddfea8cb819e57a102b12f49d8246be80d695b
Daryna Holikova <dgolikova0@gmail.com>
2024-12-31 20:55:51 +0000

    add new page and all changes

--- src/app/(admin)/companies/@modal/(.)new/page.tsx
+++ src/app/(admin)/companies/@modal/(.)new/page.tsx
@@ -1,13 +1,9 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import CompanyForm from '@/src/app/components/company-form';
-import Modal from '@/src/app/components/modal';
+import CompanyFormModal from '@/src/app/components/company-form-modal';
+
 export default function Page() {
   const router = useRouter();
-  return (
-    <Modal show={true} onClose={() => router.back()}>
-      <CompanyForm onSubmit={console.log} />
-    </Modal>
-  );
+  return <CompanyFormModal show={true} onClose={() => router.back()} />;
 }
--- src/app/components/company-form-modal.tsx
+++ src/app/components/company-form-modal.tsx
@@ -1,14 +1,13 @@
 'use client';
 import React from 'react';
-import CompanyForm, { CompanyFormProps } from '@/src/app/components/company-form';
+import CompanyForm from '@/src/app/components/company-form';
+
 import Modal, { ModalProps } from '@/src/app/components/modal';
-export interface CompanyFormModalProps extends ModalProps {
-    onSubmit: CompanyFormProps['onSubmit'];
-}
-export default function CompanyFormModal({onSubmit, ...rest}: CompanyFormModalProps) {
-    return (
-        <Modal {...rest}>
-            <CompanyForm onSubmit={onSubmit} />
-        </Modal>
-    );
+
+export default function CompanyFormModal({ onClose, ...rest }: ModalProps) {
+  return (
+    <Modal {...rest} onClose={onClose}>
+      <CompanyForm onSubmit={() => onClose()} />
+    </Modal>
+  );
 }
