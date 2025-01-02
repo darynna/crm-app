0cac56ded8ed47e0782c9ecf89c863086bb3d59f
Daryna Holikova <dgolikova0@gmail.com>
2025-01-02 20:20:25 +0000

    made changes to prevent error

--- src/app/components/promotion-form.tsx
+++ src/app/components/promotion-form.tsx
@@ -44,6 +44,10 @@ export default function PromotionForm({
     },
   });
   const handleSubmit = async (values: PromotionFieldValues) => {
+    if (!company) {
+      console.error("Company data is not available.");
+      return;
+    }
     await mutateAsync({
       ...values,
       discount: Number(values.discount) || 0,
